2ff1e2773357016c5ba8b4201d5f8e582621eefc
Riva Almero <rvalms@icloud.com>
2025-08-08 22:49:19 +0700

    fix: auth middleware

--- middleware/admin-auth.global.ts
+++ middleware/admin-auth.global.ts
@@ -9,21 +9,27 @@ export default defineNuxtRouteMiddleware((to, from) => {
       (route) => route === to.path
    )
 
+   // NOTE: If user is logged in AND trying to access guest page
    if (token && !isAdminRoute) {
       return navigateTo("/admin", { replace: true })
    }
 
-   if (token && isTokenExpired) {
-      authStore.$reset()
-      useAppStore().notify("Warning", "Token kedaluwarsa")
-      return navigateTo("/admin/login", { replace: true })
-   }
+   if (isAdminRoute) {
+      // NOTE: If user is not logged in AND trying to access admin page
+      if (!token && !userIsInIgnoredRoutes) {
+         return navigateTo("/admin/login", { replace: true })
+      }
 
-   if (!token && !userIsInIgnoredRoutes) {
-      return navigateTo("/admin/login", { replace: true })
-   }
+      // NOTE: If token expired
+      if (isTokenExpired && !userIsInIgnoredRoutes) {
+         authStore.$reset()
+         useAppStore().notify("Warning", "Token kedaluwarsa")
+         return navigateTo("/admin/login", { replace: true })
+      }
 
-   if (token && userIsInIgnoredRoutes) {
-      return navigateTo("/admin", { replace: true })
+      // NOTE: If user is logged in AND trying to access ignored page
+      if (token && userIsInIgnoredRoutes) {
+         return navigateTo("/admin", { replace: true })
+      }
    }
 })
